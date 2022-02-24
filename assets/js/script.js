f85f213e826a7861b3241fee99466a5fddb7cd18
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-02-23 22:33:35 -0700

    stopped page from refreshing on submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,17 @@
-console.dir(window.document);
-
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-buttonEl.addEventListener("click", function() {
-});
-
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+
+  event.preventDefault();
+  console.log(event);
+
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
