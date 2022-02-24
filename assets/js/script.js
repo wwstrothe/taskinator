85fbb510e9c00d2222754cdb3a4d1f36413e10e1
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-02-23 23:05:12 -0700

    updated script.js to add tasks to list

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,22 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-
   event.preventDefault();
-  console.log(event);
-
-
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // Create List Item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // Create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  
+  // add HTML content to Div 
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add Line Item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
