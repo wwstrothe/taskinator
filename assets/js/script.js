d33675453976ab7d3866cde7d332d462e7653891
Strothe <williamstrothe@rocketmortgage.com>
2022-02-23 21:54:59 -0700

    created working button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
 console.dir(window.document);
+
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+buttonEl.addEventListener("click", function() {
+  alert("button clicked");
+});
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
+
